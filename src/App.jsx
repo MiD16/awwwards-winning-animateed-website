59fbd7b35bff25fe8ed375321287ef6dad9fdd33
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-07-30 11:29:07 +0300

    Initialize Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='text-5xl text-violet-300 font-bold'>
+      <h1>Welcome</h1>
+    </main>
+  )
+}
+
+export default App
