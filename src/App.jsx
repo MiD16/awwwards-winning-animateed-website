1b19df0be1664f6f8eb147ca7d37440205464bd3
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-08-02 12:01:55 +0300

    Contact + Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import About from './components/About'
 import NavBar from './components/NavBar';
 import Features from './components/Features';
 import Story from './components/Story';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -17,6 +19,8 @@ const App = () => {
       <About />
       <Features />
       <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
